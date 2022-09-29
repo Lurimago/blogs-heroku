1476914d13e180f4250d38017fe624d817a69bc2
Lurimago <luisricardomarcelogomez@gmail.com>
2022-09-29 00:27:55 -0500

    Deploy

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 // Routers
-const { usersRouter } = require('./routes/users.routes');
-const { postsRouter } = require('./routes/posts.routes');
-const { commentsRouter } = require('./routes/comments.routes');
+const { usersRouter } = require("./routes/users.routes");
+const { postsRouter } = require("./routes/posts.routes");
+const { commentsRouter } = require("./routes/comments.routes");
 
 // Controllers
-const { globalErrorHandler } = require('./controllers/error.controller');
+const { globalErrorHandler } = require("./controllers/error.controller");
 
 // Init our Express app
 const app = express();
@@ -14,21 +17,29 @@ const app = express();
 // Enable Express app to receive JSON data
 app.use(express.json());
 
+// Add security headers
+app.use(helmet());
+
+// Compress responses
+app.use(compression());
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else if (process.env.NODE_ENV === "production") app.use(morgan("combined"));
+
 // Define endpoints
-// /posts
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/posts', postsRouter); // next(error)
-app.use('/api/v1/comments', commentsRouter);
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/comments", commentsRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
 
 // Catch non-existing endpoints
-app.all('*', (req, res) => {
-	res.status(404).json({
-		status: 'error',
-		message: `${req.method} ${req.url} does not exists in our server`,
-	});
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: `${req.method} ${req.url} does not exists in our server`,
+  });
 });
 
 module.exports = { app };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 // Controllers
 const {
-	getAllUsers,
-	createUser,
-	updateUser,
-	deleteUser,
-	login,
-} = require('../controllers/users.controller');
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+} = require("../controllers/users.controller");
 
 // Middlewares
-const { userExists } = require('../middlewares/users.middlewares');
+const { userExists } = require("../middlewares/users.middlewares");
 const {
-	protectSession,
-	protectUsersAccount,
-	protectAdmin,
-} = require('../middlewares/auth.middlewares');
+  protectSession,
+  protectUsersAccount,
+  protectAdmin,
+} = require("../middlewares/auth.middlewares");
 const {
-	createUserValidators,
-} = require('../middlewares/validators.middlewares');
+  createUserValidators,
+} = require("../middlewares/validators.middlewares");
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', createUserValidators, createUser);
+usersRouter.post("/", createUserValidators, createUser);
 
-usersRouter.post('/login', login);
+usersRouter.post("/login", login);
 
 // Protecting below endpoints
-usersRouter.use(protectSession);
+// usersRouter.use(protectSession);
 
-usersRouter.get('/', protectAdmin, getAllUsers);
+usersRouter.get("/", protectAdmin, getAllUsers);
 
-usersRouter.patch('/:id', userExists, protectUsersAccount, updateUser);
+usersRouter.patch("/:id", userExists, protectUsersAccount, updateUser);
 
-usersRouter.delete('/:id', userExists, protectUsersAccount, deleteUser);
+usersRouter.delete("/:id", userExists, protectUsersAccount, deleteUser);
 
 module.exports = { usersRouter };
